0266b5546b79beec486725dea6e82ead928d41c2
Stephen Clemmer <stephenjclemmer@gmail.com>
2023-01-06 20:15:33 -0800

    added text fields to the contact component

--- src/contact.js
+++ src/contact.js
@@ -1,23 +1,92 @@
 import Paper from '@mui/material/Paper';
 import Container from '@mui/material/Container';
-import Button from '@mui/material/Button'
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import Button from '@mui/material/Button';
+import KeyboardArrowRightOutlined from '@mui/icons-material/KeyboardArrowRightOutlined';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import { React, useState } from 'react';
+import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
+
+// import { makeStyles } from 'tss-react/mui';
+
+// const useStyles = makeStyles({
+//   field: {
+//     marginTop: 20,
+//     marginBottom: 20,
+//     display: 'block'
+//   }
+// })
 
 export default function Welcome() {
+  //   const classes = useStyles()
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (name && email) {
+      console.log('Form has been \'submitted\'')
+    }
+  }
+
   return (
 
     <Container>
-      <Paper
-        elevation={0}> This should be turned into a contact form
-      </Paper>
-      <Button 
-        onClick={() => console.log('you pushed it')}
-        variant="contained"
-        color="primary"
-        type="submit"
+      <form
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit}
       >
-        <MailOutlineIcon />
-      </Button>
+        <Grid>
+          <TextField
+            // className={classes.field} 
+            onChange={(e) => setName(e.target.value)}
+            label="Name"
+            variant="outlined"
+            color="secondary"
+            required
+          />
+        </Grid>
+        <Grid>
+          <TextField
+            // className={classes.field} 
+            onChange={(e) => setEmail(e.target.value)}
+            label="Email"
+            variant="outlined"
+            color="secondary"
+            required
+          />
+        </Grid>
+        <Grid>
+          <TextField
+            // className={classes.field}
+            onChange={(e) => setMessage(e.target.value)}
+            label="Message"
+            variant="outlined"
+            color="secondary"
+            required
+            multiline
+            rows='4'
+          />
+        </Grid>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+        >Submit
+          <KeyboardArrowRightOutlined />
+        </Button>
+
+      </form>
+
+      <Typography>
+        or email me at StephenJClemmer@gmail.com
+      </Typography>
+
     </Container>
 
   );
